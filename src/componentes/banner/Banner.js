dae98c163b4b30088306e52a32ebf216ff8de17e
vinicius-rosa-magio <105308924+vinicius-rosa-magio@users.noreply.github.com>
2022-07-25 15:50:55 -0300

    primeiro commit

---
+++ src/componentes/banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagem/banner.png" alt="banner" />
+        </header>
+    )
+}
